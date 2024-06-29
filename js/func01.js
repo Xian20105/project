442ef34ae171ef6a456d91d7aca74908f0de5dff
Xian20105 <xian20105@gmail.com>
2024-06-30 02:47:31 +0800

    update

--- js/func01.js
+++ js/func01.js
@@ -1,20 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var menuToggle = document.getElementById('menuToggle');
-  var menu = document.getElementById('menu');
+  var menuToggle = document.getElementById('menu-icon');
+  var menu = document.querySelector('.drop-down-menu');
+  var dropDownIcon = document.getElementById('drop-down-icon');
+
+  var touchStartY = 0;
+  var touchEndY = 0;
 
   menuToggle.addEventListener('click', function() {
       menu.classList.toggle('show');
   });
 
   document.addEventListener('click', function(event) {
-      var isClickInside = menu.contains(event.target) || menuToggle.contains(event.target);
+      var isClickInside = menu.contains(event.target) || menuToggle.contains(event.target) || dropDownIcon.contains(event.target);
 
       if (!isClickInside) {
           menu.classList.remove('show');
       }
   });
 
-  document.addEventListener('touchmove', function() {
-      menu.classList.remove('show');
+
+  document.addEventListener('touchstart', function(event) {
+      touchStartY = event.touches[0].clientY;
   });
+
+
+  document.addEventListener('touchmove', function(event) {
+      touchEndY = event.touches[0].clientY;
+      handleTouchMove();
+  });
+
+
+  function handleTouchMove() {
+      var touchDistance = Math.abs(touchStartY - touchEndY);
+
+      if (touchDistance > 10) { 
+          menu.classList.remove('show');
+      }
+  }
 });
